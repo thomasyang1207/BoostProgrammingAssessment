6e7a77f44b4e0205b7fcc832e8b155d226c13a67
Thomas Yang <thomasyang127@gmail.com>
2019-03-24 22:16:06 -0500

    Initial commit

---
+++ matrix.h
@@ -0,0 +1,18 @@
+//Matrix definitions here...
+
+
+template <template T> // T is generic "numeric" data type... 
+class matrix {
+	public:
+
+		matrix(); //constructor
+
+		//Overload the +, -, = operators; 
+
+	private:
+		//Where I want the internal representation to go
+
+
+};
+
+
